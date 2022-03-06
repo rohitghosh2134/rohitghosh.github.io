2eb62a82d90d139d688b544535308899f5a0bfd7
rohitghosh2134 <rohitghoshkol8@gmail.com>
2022-03-06 18:14:19 +0530

    Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-
+const navMenu=document.getElementById('nav-menu'),
+    navToggle=document.getElementById('nav-toggle'),
+    navClose=document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
@@ -7,7 +9,11 @@
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if(navToggle){
+    navToggle.addEventListener('click',()=>{
+        navMenu.classList.add('show-menu')
+    })
+}
 
 /*==================== REMOVE MENU MOBILE ====================*/
 
