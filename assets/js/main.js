df74456b3d37b7b4615bc873a84ca86806335958
rohitghosh2134 <rohitghoshkol8@gmail.com>
2022-03-07 12:02:27 +0530

    Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,8 +29,22 @@ function linkAction(){
 navLink.forEach(n=>n.addEventListener('click',linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent=document.getElementById('skills_content'),
+    skillsHeader=document.getElementById('.skills_header')
 
-
+function toggleSkills(){
+    let itemClass=this.parentNode.getElementByClassName
+    for(i=0;i<skillsContent.length;i++)
+    {
+        skillsContent[i].className='skills_content skills_close'
+    }
+    if(itemClass==='skills_content skills_close'){
+        this.parentNode.className='skills_content skills_open'
+    }
+}
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
 /*==================== QUALIFICATION TABS ====================*/
 
 
