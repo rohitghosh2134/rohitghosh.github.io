1f66a658f3ec72863ed647b44ec43a7a0f594191
rohitghosh2134 <rohitghoshkol8@gmail.com>
2022-03-09 17:35:39 +0530

    Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -89,6 +89,19 @@ modalCloses.forEach((modalClose)=>{
     })
 })
 /*==================== PORTFOLIO SWIPER  ====================*/
+var swiper = new Swiper('.portfolio_container', {
+    cssMode: true,
+    loop:true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable:true,
+    },
+    });
 
 
 /*==================== TESTIMONIAL ====================*/
